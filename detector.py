6713b5ac6ccd9d0644c0a7bfa1ce08010aed789d
Bruno Cruz <cruzzz.1998@live.com.pt>
2022-02-08 15:32:43 +0000

    Update

--- detector.py
+++ detector.py
@@ -24,40 +24,32 @@ def Menu():
         exit(0)
     elif choice == 1:
             
-        # Open Choose File Dialog #
         Tk().withdraw()
         filename = askopenfilename(filetypes=[("Images", ".jpg .png")])
 
         if filename == "":
             Menu()
 
-        # Import Images #
         img = cv2.imread(filename)
 
-        # Convert Imported Images To Grayscale #
         grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Detect Faces
-        face_coordinates = trained_data.detectMultiScale(grayscaled_img)
+        coordinates = trained_data.detectMultiScale(grayscaled_img)
 
-        # Loop Throw All Detected Faces #
-        for (x, y, w, h) in face_coordinates:
+        for (x, y, w, h) in coordinates:
             cv2.rectangle(img , (x, y), (x+w, y+h), (0, 255, 0), 1)
 
-        # Display The Image With The Faces #
         cv2.imshow('AI Detector (Image)', img)
         cv2.waitKey()
 
     elif choice == 2:
 
-        # Open Choose File Dialog #
         Tk().withdraw()
         filename = askopenfilename(filetypes=[("Videos", ".mp4 .mov .webm .gif")])
 
         if filename == "":
             Menu()
             
-        #Import Video
         video = cv2.VideoCapture(filename)
 
         print("Press Q to exit")
@@ -68,9 +60,9 @@ def Menu():
 
             grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            face_coordinates = trained_data.detectMultiScale(grayscaled_img)
+            coordinates = trained_data.detectMultiScale(grayscaled_img)
 
-            for(x, y, w, h) in face_coordinates:
+            for(x, y, w, h) in coordinates:
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
 
             cv2.imshow('AI Detector (Video)', frame)
@@ -81,7 +73,6 @@ def Menu():
 
     elif choice == 3:
 
-        #Import Cam
         cam = cv2.VideoCapture(0)
 
         print("Press Q to exit")
@@ -92,9 +83,9 @@ def Menu():
 
             grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            face_coordinates = trained_data.detectMultiScale(grayscaled_img)
+            coordinates = trained_data.detectMultiScale(grayscaled_img)
 
-            for(x, y, w, h) in face_coordinates:
+            for(x, y, w, h) in coordinates:
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
 
             cv2.imshow('AI Detector (Cam)', frame)
